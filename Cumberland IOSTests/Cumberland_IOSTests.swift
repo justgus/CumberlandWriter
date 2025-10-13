011bed6de2c4017e00fc57bbe75c0971ec6dfa98
justgus <justgus@me.com>
2025-10-13 17:42:45 -0400

    periodic checkin

---
+++ Cumberland IOSTests/Cumberland_IOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Cumberland_IOSTests.swift
+//  Cumberland IOSTests
+//
+//  Created by Mike Stoddard on 10/13/25.
+//
+
+import Testing
+@testable import Cumberland_IOS
+
+struct Cumberland_IOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
