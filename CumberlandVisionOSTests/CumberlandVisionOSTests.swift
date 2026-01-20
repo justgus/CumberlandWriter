421169b2a842fe23edcb56131c62d65ebd5816eb
justgus <justgus@me.com>
2026-01-20 15:12:35 -0500

    Add CI/CD test workflow

---
+++ CumberlandVisionOSTests/CumberlandVisionOSTests.swift
@@ -0,0 +1,16 @@
+//
+//  CumberlandVisionOSTests.swift
+//  CumberlandVisionOSTests
+//
+//  Created by Mike Stoddard on 1/20/26.
+//
+
+import Testing
+
+struct CumberlandVisionOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
